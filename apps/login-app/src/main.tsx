4706650facce86060941b2895a294c703172bfc1
Hiroki Yoshida <yoshida@upward.jp>
2025-05-12 00:18:17 +0900

    refactor: Lintエラーの修正

--- apps/login-app/src/main.tsx
+++ apps/login-app/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./pages/home/home.tsx"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root")
+if (rootElement) {
+	createRoot(rootElement).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	)
+} else {
+	console.error("Root element not found")
+}
