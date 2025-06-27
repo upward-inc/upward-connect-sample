5e12bfcb52a4c13a68daf209fe03b81a0151e48e
Hiroki Yoshida <yoshida@upward.jp>
2025-06-27 16:25:18 +0900

    feat: シードスクリプトの調整
    ハッシュ化されたパスワードを設定

--- apps/api/seed/user.ts
+++ apps/api/seed/user.ts
@@ -2,25 +2,30 @@ import type { Prisma } from "@prisma/client"
 import { getRandomBoolean, getUniquePersons } from "./utility"
 
 export async function seedUsers(prisma: Prisma.TransactionClient) {
-	const users: Prisma.userCreateManyInput[] = getUniquePersons(50)
-		.map(({ firstName, lastName }) => {
-			return {
-				userName: `${firstName.roman}-${lastName.roman}`,
-				firstName: firstName.value,
-				lastName: lastName.value,
-			}
-		})
-		.map(({ userName, firstName, lastName }) => {
-			return {
-				user_name: userName,
-				first_name: firstName,
-				last_name: lastName,
-				email: `${userName}@example.com`,
-				timezone: "Asia/Tokyo",
-				language: "ja",
-				is_active: getRandomBoolean(0.9),
-			}
-		})
+	const users: Prisma.userCreateManyInput[] = await Promise.all(
+		getUniquePersons(50)
+			.map(({ firstName, lastName }) => {
+				return {
+					userName: `${firstName.roman}-${lastName.roman}`,
+					firstName: firstName.value,
+					lastName: lastName.value,
+				}
+			})
+			.map(async ({ userName, firstName, lastName }) => {
+				const email = `${userName}@example.com`
+				const hashedPassowrd = await Bun.password.hash(userName)
+				return {
+					user_name: email,
+					hashed_password: hashedPassowrd,
+					first_name: firstName,
+					last_name: lastName,
+					email,
+					timezone: "Asia/Tokyo",
+					language: "ja",
+					is_active: getRandomBoolean(0.9),
+				}
+			}),
+	)
 
 	const records = await prisma.user.createMany({ data: users })
 
