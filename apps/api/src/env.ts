eae0568dd67715db6009e409b592eb33b5c967d2
Hiroki Yoshida <yoshida@upward.jp>
2025-06-29 16:46:11 +0900

    feat: 環境変数の整理

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -4,8 +4,10 @@ const envSchema = z.object({
 	PORT: z.coerce.number().min(1000).max(65535),
 	OIDC_ISSUER: z.string().url().startsWith("https://"),
 	OIDC_AUDIENCE: z.string(),
-	OIDC_JWT_SECRET: z.string().min(10),
-	OIDC_JWT_EXPIRES_IN_SECOND: z.coerce.number().min(1),
+	OIDC_TOKEN_SECRET: z.string().min(10),
+	OIDC_TOKEN_EXPIRES_IN_MINUTE: z.coerce.number().min(1),
+	OIDC_REFRESH_TOKEN_SECRET: z.string().min(10),
+	OIDC_REFRESH_TOKEN_EXPIRES_IN_DAY: z.coerce.number().min(1),
 })
 
 export const env = envSchema.parse(process.env)
