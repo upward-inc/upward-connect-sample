8ac2ffcff7e39965c400ebc09858908607ac12d0
Hiroki Yoshida <yoshida@upward.jp>
2025-06-29 16:46:39 +0900

    feat: トークン生成関数の定義

---
+++ apps/api/src/domain/auth/generate-token.ts
@@ -0,0 +1,57 @@
+import { sign } from "jsonwebtoken"
+import { env } from "../../env"
+
+/**
+ * ユーザー情報からアクセストークンとIDトークンを生成する
+ */
+export const generateToken = (user: {
+	id: string
+	userName: string
+}) => {
+	const accessToken = sign(
+		{
+			username: user.userName,
+		},
+		env.OIDC_TOKEN_SECRET,
+		{
+			algorithm: "HS256",
+			issuer: env.OIDC_ISSUER,
+			subject: user.id,
+			audience: env.OIDC_AUDIENCE,
+			expiresIn: `${env.OIDC_TOKEN_EXPIRES_IN_MINUTE} minutes`,
+		},
+	)
+
+	const idToken = sign(
+		{
+			username: user.userName,
+		},
+		env.OIDC_TOKEN_SECRET,
+		{
+			algorithm: "HS256",
+			issuer: env.OIDC_ISSUER,
+			subject: user.id,
+			audience: env.OIDC_AUDIENCE,
+			expiresIn: `${env.OIDC_TOKEN_EXPIRES_IN_MINUTE} minutes`,
+		},
+	)
+
+	return { accessToken, idToken }
+}
+
+/**
+ * ユーザー情報からリフレッシュトークンを生成する
+ */
+export const generateRefreshToken = (user: {
+	id: string
+}) => {
+	const refreshToken = sign({}, env.OIDC_REFRESH_TOKEN_SECRET, {
+		algorithm: "HS256",
+		issuer: env.OIDC_ISSUER,
+		subject: user.id,
+		audience: env.OIDC_AUDIENCE,
+		expiresIn: `${env.OIDC_REFRESH_TOKEN_EXPIRES_IN_DAY} days`,
+	})
+
+	return { refreshToken }
+}
--- apps/api/src/domain/auth/index.ts
+++ apps/api/src/domain/auth/index.ts
@@ -1,1 +1,2 @@
 export * from "./verify-user"
+export * from "./generate-token"
