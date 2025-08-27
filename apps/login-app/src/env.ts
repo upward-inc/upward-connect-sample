910f0237002ae764dd815691bedc0ca9ed9d31d6
b02902092 <b02902092@ntu.edu.tw>
2025-08-27 10:34:27 +0800

    Add login_deploy_dev.yml

--- apps/login-app/src/env.ts
+++ apps/login-app/src/env.ts
@@ -1,13 +1,16 @@
 import { z } from "zod"
 
 const envSchema = z.object({
-	PORT: z.coerce.number().min(1000).max(65535),
 	API_URL: z
 		.string()
 		.url()
 		.regex(/^(https:\/\/|http:\/\/localhost(:[0-9]+)?(\/.*)?$)/), // start with https:// or http://localhost:[port]
 })
 
-export const env = envSchema.parse(process.env)
+const processedEnv = {
+	API_URL: import.meta.env.VITE_API_URL,
+}
+
+export const env = envSchema.parse(processedEnv)
 
 export type Environment = z.infer<typeof envSchema>
--- apps/login-app/vite.config.ts
+++ apps/login-app/vite.config.ts
@@ -1,27 +1,15 @@
 import tailwindcss from "@tailwindcss/vite"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), "")
-
+export default defineConfig(() => {
 	return {
 		plugins: [
 			TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
 			react(),
 			tailwindcss(),
 		],
-		define: {
-			...Object.keys(env).reduce(
-				(prev: Record<string, string>, key) => {
-					prev[`process.env.${key}`] = JSON.stringify(env[key])
-
-					return prev
-				},
-				{} as Record<string, string>,
-			),
-		},
 	}
 })
