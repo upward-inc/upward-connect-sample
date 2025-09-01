35066943fc10f9a5deefbaa57f0bb6567e23fdea
b02902092 <b02902092@ntu.edu.tw>
2025-09-01 14:53:55 +0900

    Let schema be optional in describeRoute()

--- apps/api/src/libs/hono-openapi.ts
+++ apps/api/src/libs/hono-openapi.ts
@@ -4,35 +4,24 @@ import type { ZodType } from "zod"
 
 export const describeRoute = <S extends ZodType>(options: {
 	description: string
-	schema: S
+	schema?: S
 }) => {
 	const { description, schema } = options
+	// schema がある場合は、レスポンスの validation を行う
+	const content = schema
+		? {
+				"application/json": {
+					schema: honoOpenApiZod.resolver(schema),
+				},
+			}
+		: undefined
 	return honoOpenApi.describeRoute({
 		description,
 		validateResponse: true,
 		responses: {
 			200: {
 				description: "Success",
-				content: {
-					"application/json": {
-						schema: honoOpenApiZod.resolver(schema),
-					},
-				},
-			},
-		},
-	})
-}
-
-export const describeRouteWithoutResponseValidation = (options: {
-	description: string
-}) => {
-	const { description } = options
-	return honoOpenApi.describeRoute({
-		description,
-		validateResponse: false,
-		responses: {
-			200: {
-				description: "Success",
+				content: content,
 			},
 		},
 	})
