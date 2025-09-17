0402e922a8702167391751b7e85f2d3b05d38052
b02902092 <b02902092@ntu.edu.tw>
2025-09-17 16:19:46 +0900

    Fix wrong example filed name web_site to website

--- apps/api/src/schema/record.ts
+++ apps/api/src/schema/record.ts
@@ -65,7 +65,7 @@ export const CreateRecordDataSchema = z
 			account_number: "ACC-XXXXX",
 			main_phone_number: "03-1234-5678",
 			sub_phone_number: null,
-			web_site: "https://www.example.com",
+			website: "https://www.example.com",
 			industry: "agriculture", // single optional
 			number_of_employees: 1000, // numeric
 			revenue: 10000000,
