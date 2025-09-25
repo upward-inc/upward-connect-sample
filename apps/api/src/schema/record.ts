094ea56870fd2296ed71aff577a517bc7bcfb6b8
b02902092 <b02902092@ntu.edu.tw>
2025-09-25 15:38:56 +0900

    Fix uri and request body for create records endpoint

--- apps/api/src/schema/record.ts
+++ apps/api/src/schema/record.ts
@@ -52,7 +52,14 @@ export const GetRecordListResponseSchema = z.object({
 	data: RecordListSchema.openapi({ description: "クエリの結果を表す配列" }),
 })
 
-export const PostRecordDataSchema = z
+export const PostRecordParamSchema = z.object({
+	entity_name: z.string().openapi({
+		description: "作成対象のエンティティ名",
+		examples: ["account", "lead"],
+	}),
+})
+
+export const PostRecordBodySchema = z
 	.any()
 	.refine((data) => {
 		// Ensure it's a non-null json object
@@ -78,14 +85,6 @@ export const PostRecordDataSchema = z
 			parent: "fe604faa-0731-4424-a22f-737d60047f39", // reference
 		},
 	})
-
-export const PostRecordBodySchema = z.object({
-	entity_name: z.string().openapi({
-		description: "作成対象のエンティティ名",
-		examples: ["account", "lead"],
-	}),
-	data: PostRecordDataSchema,
-})
 
 export const PostRecordResponseSchema = z
 	.object({
