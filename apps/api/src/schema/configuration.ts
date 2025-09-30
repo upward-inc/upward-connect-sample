8e6ea1c7b1bbf1928f87d7b021405f5c445646cc
yamachan0625 <55592297+yamachan0625@users.noreply.github.com>
2025-09-30 15:57:11 +0900

    description修正

--- apps/api/src/schema/configuration.ts
+++ apps/api/src/schema/configuration.ts
@@ -3,37 +3,32 @@ import "zod-openapi/extend"
 
 export const ConfigurationSchema = z
 	.object({
-		entity_name: z
-			.object({
-				user: z.string().openapi({
-					description: "ユーザーエンティティの名称",
-					example: "user",
-				}),
-				account: z.string().openapi({
-					description: "アカウントエンティティの名称",
-					example: "account",
-				}),
-				lead: z.string().openapi({
-					description: "リードエンティティの名称",
-					example: "lead",
-				}),
-				contact: z.string().openapi({
-					description: "コンタクトエンティティの名称",
-					example: "contact",
-				}),
-				activity: z.string().openapi({
-					description: "アクティビティエンティティの名称",
-					example: "activity",
-				}),
-				phone_call: z.string().openapi({
-					description: "電話エンティティの名称",
-					example: "phone_call",
-				}),
-			})
-			.openapi({
-				description:
-					"UPWARDの標準エンティティ名と顧客側のエンティティ名のマッピング",
+		entity_name: z.object({
+			user: z.string().openapi({
+				description: "ユーザーエンティティの名称",
+				example: "user",
 			}),
+			account: z.string().openapi({
+				description: "アカウントエンティティの名称",
+				example: "account",
+			}),
+			lead: z.string().openapi({
+				description: "リードエンティティの名称",
+				example: "lead",
+			}),
+			contact: z.string().openapi({
+				description: "コンタクトエンティティの名称",
+				example: "contact",
+			}),
+			activity: z.string().openapi({
+				description: "アクティビティエンティティの名称",
+				example: "activity",
+			}),
+			phone_call: z.string().openapi({
+				description: "電話エンティティの名称",
+				example: "phone_call",
+			}),
+		}),
 	})
 	.openapi({
 		description: "構成データの説明",
