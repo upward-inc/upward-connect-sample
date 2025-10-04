87b93ca0a8796ab5b7d6b01730f516f8528d7730
Hiroki Yoshida <yoshida@upward.jp>
2025-10-04 20:36:47 +0900

    Zodスキーマの調整（profile, role）

--- apps/api/src/schema/role.ts
+++ apps/api/src/schema/role.ts
@@ -4,23 +4,25 @@ import "zod-openapi/extend"
 export const RoleSchema = z
 	.object({
 		name: z.string().openapi({
-			description: "名称",
+			description: "ロール名",
 			example: "east_japan_sales_department",
 		}),
 		display_name: z.string().openapi({
-			description: "表示名",
-			example: "東日本営業部 ",
+			description: "ロールの表示名",
+			example: "東日本営業部",
 		}),
 		parent_name: z.string().nullish().openapi({
-			description: "親ロールの名称",
+			description: "親ロール名",
 			example: "sales_department",
 		}),
 	})
 	.openapi({
-		description: "ロールの説明",
+		description: "ロール",
 	})
 
-export const RoleListSchema = z.array(RoleSchema)
+export const RoleListSchema = z.array(RoleSchema).openapi({
+	description: "ロール一覧",
+})
 
 export const GetRoleParamSchema = z.object({
 	name: z.string(),
