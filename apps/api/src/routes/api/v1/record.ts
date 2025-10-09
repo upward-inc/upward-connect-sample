3816070652582f838a81e350df841acbae895aa7
yamachan0625 <55592297+yamachan0625@users.noreply.github.com>
2025-10-09 12:26:37 +0900

    バリデーション追加

--- apps/api/src/routes/api/v1/record.ts
+++ apps/api/src/routes/api/v1/record.ts
@@ -5,6 +5,7 @@ import {
 	deleteRecord,
 	getRecordList,
 	validateCreateRecordBody,
+	validateGetRecordListQuery,
 } from "../../../domain/record"
 import { describeRoute, validator } from "../../../libs/hono-openapi"
 import type { AuthContexts } from "../../../schema/auth"
@@ -36,6 +37,11 @@ export const recordRouter = new Hono<{ Variables: AuthContexts }>()
 			if (!entity) {
 				const message = `Entity '${entity_name}' does not exist`
 				return c.json({ message }, 404)
+			}
+
+			const validateResult = validateGetRecordListQuery(query)
+			if (!validateResult.success) {
+				return c.json({ message: validateResult.message }, 400)
 			}
 
 			const { data, has_next_page, total_size } = await getRecordList(
