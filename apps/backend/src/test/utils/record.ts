c0b8ebba691c903ead383dab5ab964e545c6206f
Hiroki Yoshida <yoshida@upward.jp>
2025-10-21 16:41:31 +0900

    マージミスの修正

--- apps/backend/src/test/utils/record.ts
+++ apps/backend/src/test/utils/record.ts
@@ -67,13 +67,8 @@ interface RecordReference {
  */
 export async function createTestAccount(
 	userId: string,
-<<<<<<< HEAD:apps/backend/src/test/utils/record.ts
-) {
-	const userReference = JSON.stringify({ entity_name: "user", id: userId })
-=======
 	data: AccountData,
 ): Promise<Account> {
->>>>>>> f1675f3 (テストレコードデータ操作ユーティリティの作成):apps/api/src/test/utils/record.ts
 	const account = await testPrisma.account.create({
 		data: toAccountCreateInput(userId, data),
 		select: { id: true },
@@ -124,13 +119,8 @@ export async function deleteTestAccountsByPrefix(prefix: string) {
  */
 export async function createTestLead(
 	userId: string,
-<<<<<<< HEAD:apps/backend/src/test/utils/record.ts
-) {
-	const userReference = JSON.stringify({ entity_name: "user", id: userId })
-=======
 	data: LeadData,
 ): Promise<Lead> {
->>>>>>> f1675f3 (テストレコードデータ操作ユーティリティの作成):apps/api/src/test/utils/record.ts
 	const lead = await testPrisma.lead.create({
 		data: toLeadCreateInput(userId, data),
 		select: { id: true },
@@ -146,240 +136,43 @@ export async function createTestLead(
  */
 export async function createManyTestLeads(
 	userId: string,
-<<<<<<< HEAD:apps/backend/src/test/utils/record.ts
-) {
-	const userReference = JSON.stringify({ entity_name: "user", id: userId })
-	const activity = await testPrisma.activity.create({
-		data: {
-			subject: `test_${activityData.subject}`,
-			is_all_day_event: activityData.is_all_day_event,
-			is_archived: activityData.is_archived,
-			owner: userReference,
-			created_by: userReference,
-			modified_by: userReference,
-		},
-		select: {
-			id: true,
-		},
-=======
 	data: Array<LeadData>,
 ): Promise<{ count: number }> {
 	return await testPrisma.lead.createMany({
 		data: data.map((d) => toLeadCreateInput(userId, d)),
->>>>>>> f1675f3 (テストレコードデータ操作ユーティリティの作成):apps/api/src/test/utils/record.ts
 	})
 }
 
 /**
  * リードテストデータの全削除
  */
-<<<<<<< HEAD:apps/backend/src/test/utils/record.ts
-export async function createTestPhoneCall(
-	phoneCallData: {
-		subject: string
-		user: { entity_name: string; id: string }
-		their: { entity_name: string; id: string }
-		direction: "inbound" | "outbound"
-	},
-	userId: string,
-) {
-	const userReference = JSON.stringify({ entity_name: "user", id: userId })
-	const phoneCall = await testPrisma.phone_call.create({
-		data: {
-			subject: `test_${phoneCallData.subject}`,
-			user: JSON.stringify(phoneCallData.user),
-			their: JSON.stringify(phoneCallData.their),
-			direction: JSON.stringify([phoneCallData.direction]),
-			status: JSON.stringify(["connected"]), // Add status field to avoid null/undefined issues
-			owner: userReference,
-			created_by: userReference,
-			modified_by: userReference,
-		},
-		select: {
-			id: true,
-		},
-	})
-
-	return phoneCall
-=======
 export async function deleteAllTestLeads() {
 	await testPrisma.lead.deleteMany()
->>>>>>> f1675f3 (テストレコードデータ操作ユーティリティの作成):apps/api/src/test/utils/record.ts
 }
 
 /**
  * リードテストデータの削除（レコードID指定）
  */
-<<<<<<< HEAD:apps/backend/src/test/utils/record.ts
-export async function createTestContact(
-	contactData: {
-		last_name: string
-	},
-	userId: string,
-) {
-	const userReference = JSON.stringify({ entity_name: "user", id: userId })
-	const contact = await testPrisma.contact.create({
-		data: {
-			last_name: `test_${contactData.last_name}`,
-			owner: userReference,
-			created_by: userReference,
-			modified_by: userReference,
-		},
-		select: {
-			id: true,
-		},
-	})
-
-	return contact
-=======
 export async function deleteTestLeadById(id: string) {
 	await testPrisma.lead.delete({ where: { id } })
->>>>>>> f1675f3 (テストレコードデータ操作ユーティリティの作成):apps/api/src/test/utils/record.ts
 }
 
 /**
  * リードテストデータの一括削除（Prefix指定）
  */
-<<<<<<< HEAD:apps/backend/src/test/utils/record.ts
-export async function createTestOpportunity(
-	opportunityData: {
-		name: string
-		account: string
-		phase:
-			| "contact"
-			| "evaluation"
-			| "need_assessment"
-			| "proposal"
-			| "budget_confirmation"
-			| "price_negotiation"
-			| "proposal_creation"
-			| "final_negotiation"
-			| "deal_won"
-			| "deal_lost"
-		close_date: Date
-		is_closed: boolean
-	},
-	userId: string,
-) {
-	const userReference = JSON.stringify({ entity_name: "user", id: userId })
-	const opportunity = await testPrisma.opportunity.create({
-		data: {
-			name: `test_${opportunityData.name}`,
-			account: JSON.stringify({
-				entity_name: "account",
-				id: opportunityData.account,
-			}),
-			phase: JSON.stringify([opportunityData.phase]),
-			close_date: opportunityData.close_date,
-			is_closed: opportunityData.is_closed,
-			owner: userReference,
-			created_by: userReference,
-			modified_by: userReference,
-		},
-		select: {
-			id: true,
-		},
-=======
 export async function deleteTestLeadsByPrefix(prefix: string) {
 	await testPrisma.lead.deleteMany({
 		where: { company: { startsWith: prefix } },
->>>>>>> f1675f3 (テストレコードデータ操作ユーティリティの作成):apps/api/src/test/utils/record.ts
 	})
 }
 
 /**
-<<<<<<< HEAD:apps/backend/src/test/utils/record.ts
- * Create a test case for tests
- */
-export async function createTestCase(
-	caseData: {
-		case_number: string
-		subject: string
-	},
-	userId: string,
-) {
-	const userReference = JSON.stringify({ entity_name: "user", id: userId })
-	const testCase = await testPrisma.renamedcase.create({
-		data: {
-			case_number: `test_${caseData.case_number}`,
-			subject: `test_${caseData.subject}`,
-			owner: userReference,
-			created_by: userReference,
-			modified_by: userReference,
-		},
-		select: {
-			id: true,
-		},
-	})
-
-	return testCase
-}
-
-/**
- * Create a test product for tests
- */
-export async function createTestProduct(
-	productData: {
-		name: string
-	},
-	userId: string,
-) {
-	const userReference = JSON.stringify({ entity_name: "user", id: userId })
-	const product = await testPrisma.product.create({
-		data: {
-			name: `test_${productData.name}`,
-			owner: userReference,
-			created_by: userReference,
-			modified_by: userReference,
-		},
-		select: {
-			id: true,
-		},
-	})
-
-	return product
-}
-
-/**
- * Create a test campaign for tests
- */
-export async function createTestCampaign(
-	campaignData: {
-		name: string
-	},
-	userId: string,
-) {
-	const userReference = JSON.stringify({ entity_name: "user", id: userId })
-	const campaign = await testPrisma.campaign.create({
-		data: {
-			name: `test_${campaignData.name}`,
-			owner: userReference,
-			created_by: userReference,
-			modified_by: userReference,
-		},
-		select: {
-			id: true,
-		},
-	})
-
-	return campaign
-}
-
-/**
- * Create a test sample for tests
-=======
  * サンプルテストデータの作成
->>>>>>> f1675f3 (テストレコードデータ操作ユーティリティの作成):apps/api/src/test/utils/record.ts
  */
 export async function createTestSample(
 	userId: string,
-<<<<<<< HEAD:apps/backend/src/test/utils/record.ts
-) {
-	const userReference = JSON.stringify({ entity_name: "user", id: userId })
-=======
 	data: SampleData,
 ): Promise<Sample> {
->>>>>>> f1675f3 (テストレコードデータ操作ユーティリティの作成):apps/api/src/test/utils/record.ts
 	const sample = await testPrisma.sample.create({
 		data: toSampleCreateInput(userId, data),
 		select: { id: true },
