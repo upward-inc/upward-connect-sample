53e52ea8734686c337e4cbd289140e4a0146f73b
b02902092 <b02902092@ntu.edu.tw>
2025-10-29 15:57:37 +0900

    Fix the comparison logic in get-jwk-private-key-list.ts

--- apps/backend/src/domain/auth/get-jwk-private-key-list.ts
+++ apps/backend/src/domain/auth/get-jwk-private-key-list.ts
@@ -1,12 +1,14 @@
+import { date } from "@formkit/tempo"
 import { prisma } from "../../libs/prisma"
 import { type PrivateKeyList, PrivateKeyListSchema } from "../../schema/auth"
 
 export const getNotClosedJwkPrivateKeyList =
 	async (): Promise<PrivateKeyList> => {
+		const now = date()
 		const result = await prisma.jwk_private_key.findMany({
 			where: {
-				validate_at: { lte: new Date() },
-				closed_at: { gt: new Date() },
+				validate_at: { lte: now },
+				closed_at: { gte: now },
 			},
 		})
 
@@ -15,10 +17,11 @@ export const getNotClosedJwkPrivateKeyList =
 
 export const getNotExpiredJwkPrivateKeyList =
 	async (): Promise<PrivateKeyList> => {
+		const now = date()
 		const result = await prisma.jwk_private_key.findMany({
 			where: {
-				validate_at: { lte: new Date() },
-				expire_at: { gt: new Date() },
+				validate_at: { lte: now },
+				expire_at: { gte: now },
 			},
 		})
 		return PrivateKeyListSchema.parseAsync(result)
