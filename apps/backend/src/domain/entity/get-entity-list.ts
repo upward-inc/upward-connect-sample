15832c2ed20bbf19f478764a8069cd0596315e7b
b02902092 <b02902092@ntu.edu.tw>
2026-01-09 10:03:05 +0900

    Return id_field_name by getEntityList and getEntity

--- apps/backend/src/domain/entity/get-entity-list.ts
+++ apps/backend/src/domain/entity/get-entity-list.ts
@@ -2,8 +2,15 @@ import { prisma } from "../../libs/prisma"
 import { type EntityList, EntityListSchema } from "../../schema/entity"
 
 export const getEntityList = async (): Promise<EntityList> => {
-	const result = await prisma.entity.findMany({
-		orderBy: [{ order: "asc" }],
-	})
+	const result = await prisma.entity
+		.findMany({
+			orderBy: [{ order: "asc" }],
+		})
+		.then((results) => {
+			return results.map((result) => ({
+				...result,
+				id_field_name: "id",
+			}))
+		})
 	return EntityListSchema.parse(result)
 }
--- apps/backend/src/domain/entity/get-entity.ts
+++ apps/backend/src/domain/entity/get-entity.ts
@@ -4,9 +4,18 @@ import { type Entity, EntitySchema } from "../../schema/entity"
 export const getEntity = async (
 	name: Entity["name"],
 ): Promise<Entity | null> => {
-	const result = await prisma.entity.findUnique({
-		where: { name },
-	})
+	const result = await prisma.entity
+		.findUnique({
+			where: { name },
+		})
+		.then((result) => {
+			return result
+				? {
+						...result,
+						id_field_name: "id",
+					}
+				: null
+		})
 
 	return result ? EntitySchema.parse(result) : null
 }
