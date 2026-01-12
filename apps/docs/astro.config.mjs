da06e75dc4d3d59397814370d2160f1999809cea
Hiroki Yoshida <yoshida@upward.jp>
2026-01-12 22:01:05 +0900

    Mermaid設定定義

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,9 +1,11 @@
-// @ts-check
-import { defineConfig } from "astro/config"
 import starlight from "@astrojs/starlight"
 import tailwindcss from "@tailwindcss/vite"
+import mermaid from "astro-mermaid"
+// @ts-check
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
+// https://starlight.astro.build/reference/configuration/
 export default defineConfig({
 	integrations: [
 		starlight({
@@ -30,7 +32,19 @@ export default defineConfig({
 			],
 			customCss: ["./src/styles/global.css"],
 		}),
+
+		// https://starlight-mermaid-demo.netlify.app/configuration/
+		mermaid({
+			theme: "forest",
+			autoTheme: true,
+			mermaidConfig: {
+				flowchart: {
+					curve: "basis",
+				},
+			},
+		}),
 	],
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
