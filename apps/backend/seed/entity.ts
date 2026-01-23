e1b4abf95cc008609111d287168664c48a2fb455
b02902092 <b02902092@ntu.edu.tw>
2026-01-23 12:21:41 +0900

    Update seed/entity.ts

--- apps/backend/seed/entity.ts
+++ apps/backend/seed/entity.ts
@@ -6,25 +6,72 @@ export async function seedEntities(
 	users: user[],
 ) {
 	const entities = [
-		{ name: "user", display_name: "ユーザー", title_field_name: "full_name" },
-		{ name: "account", display_name: "取引先", title_field_name: "name" },
-		{ name: "lead", display_name: "リード", title_field_name: "full_name" },
-		{ name: "activity", display_name: "活動", title_field_name: "subject" },
-		{ name: "phone_call", display_name: "通話", title_field_name: "subject" },
+		{
+			name: "user",
+			display_name: "ユーザー",
+			title_field_name: "full_name",
+			has_location: false,
+		},
+		{
+			name: "account",
+			display_name: "取引先",
+			title_field_name: "name",
+			has_location: true,
+		},
+		{
+			name: "lead",
+			display_name: "リード",
+			title_field_name: "full_name",
+			has_location: true,
+		},
+		{
+			name: "activity",
+			display_name: "活動",
+			title_field_name: "subject",
+			has_location: true,
+		},
+		{
+			name: "phone_call",
+			display_name: "通話",
+			title_field_name: "subject",
+			has_location: false,
+		},
 		{
 			name: "contact",
 			display_name: "取引先責任者",
 			title_field_name: "full_name",
+			has_location: false,
 		},
-		{ name: "opportunity", display_name: "商談", title_field_name: "name" },
-		{ name: "case", display_name: "ケース", title_field_name: "subject" },
-		{ name: "product", display_name: "製品", title_field_name: "name" },
+		{
+			name: "opportunity",
+			display_name: "商談",
+			title_field_name: "name",
+			has_location: false,
+		},
+		{
+			name: "case",
+			display_name: "ケース",
+			title_field_name: "subject",
+			has_location: false,
+		},
+		{
+			name: "product",
+			display_name: "製品",
+			title_field_name: "name",
+			has_location: false,
+		},
 		{
 			name: "campaign",
 			display_name: "キャンペーン",
 			title_field_name: "name",
+			has_location: false,
 		},
-		{ name: "sample", display_name: "サンプル", title_field_name: "name" },
+		{
+			name: "sample",
+			display_name: "サンプル",
+			title_field_name: "name",
+			has_location: true,
+		},
 	].map((entity, index) => {
 		const user = getAnyRow(users)
 
