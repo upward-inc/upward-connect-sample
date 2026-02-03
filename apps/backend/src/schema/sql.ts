b29ec6b0be812658fc3605778d88002896d1d084
b02902092 <b02902092@ntu.edu.tw>
2026-02-03 14:02:22 +0900

    Rename function name and reference type

--- apps/backend/src/schema/sql.ts
+++ apps/backend/src/schema/sql.ts
@@ -1,7 +1,7 @@
 import { format } from "@formkit/tempo"
 import { z } from "../libs/zod"
 import { escapeStringValue } from "../utility/sql"
-import type { ComparisonSchema, RecordReferenceValue } from "./comparison"
+import type { Comparison, RecordReferenceValue } from "./comparison"
 import { EntityItemSubTypeSchema, EntityItemTypeSchema } from "./entity-item"
 import {
 	type Filter,
@@ -77,7 +77,7 @@ const getWherePredicates = (items: Field[], filter?: Filter): string | null => {
 		return null
 	}
 	if (isComparison(filter)) {
-		return baseFilterToPredicate(items, filter)
+		return comparisonToPredicate(items, filter)
 	}
 
 	if (isAndFilter(filter)) {
@@ -141,10 +141,7 @@ export const PagingClauseSchema = z
 		return [offsetClause, limitClause].filter((s) => s).join(" ")
 	})
 
-const baseFilterToPredicate = (
-	items: Field[],
-	filter: z.infer<typeof ComparisonSchema>,
-) => {
+const comparisonToPredicate = (items: Field[], filter: Comparison) => {
 	// フィールドが存在しない場合は検索条件に含めない
 	const item = items.find((item) => item.alias_name === filter.field)
 	if (!item) {
