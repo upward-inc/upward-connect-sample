096558ecfa52dce793a140f956a3de5ff8685144
b02902092 <b02902092@ntu.edu.tw>
2026-02-03 18:41:12 +0900

    Revert "Remove And/OrFilterSchema"
    This reverts commit e8c7e6cbc21de3de965ab0958c41f087836432a4.

--- apps/backend/src/schema/filter.ts
+++ apps/backend/src/schema/filter.ts
@@ -28,6 +28,30 @@ const orFilterExample: Array<TextFieldFilterSample> = [
 	{ field: "category", operator: "eq", value: "alcohol", is_not: false },
 ]
 
+// AND条件
+export const AndFilterSchema = z
+	.object({
+		and: z.array(ComparisonSchema).meta({
+			description: "条件オブジェクト配列",
+			example: andFilterExample,
+		}),
+	})
+	.meta({
+		description: "AND論理演算子による条件オブジェクト配列",
+	})
+
+// OR条件
+export const OrFilterSchema = z
+	.object({
+		or: z.array(ComparisonSchema).meta({
+			description: "条件オブジェクト配列",
+			example: orFilterExample,
+		}),
+	})
+	.meta({
+		description: "OR論理演算子による条件オブジェクト配列",
+	})
+
 // AND条件（入れ子構造可）
 export const NestableAndFilterSchema = z
 	.object({
@@ -38,6 +62,8 @@ export const NestableAndFilterSchema = z
 						NestableAndFilterSchema,
 						NestableOrFilterSchema,
 						ComparisonSchema,
+						AndFilterSchema,
+						OrFilterSchema,
 					]),
 				)
 				.meta({
@@ -68,6 +94,8 @@ export const NestableOrFilterSchema = z
 						NestableAndFilterSchema,
 						NestableOrFilterSchema,
 						ComparisonSchema,
+						AndFilterSchema,
+						OrFilterSchema,
 					]),
 				)
 				.meta({
